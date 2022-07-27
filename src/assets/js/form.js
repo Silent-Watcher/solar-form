e63cc5dfb967dc393e1e0b9beb02d800c065f27e
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-07-27 20:18:30 +0430

    add form

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,3 +1,13 @@
+const inputs = Array.from(document.getElementsByClassName('char_input'));
+const seePassIcons = Array.from(
+  document.getElementsByClassName('see_Password')
+);
+const rememberMeInputs = document.getElementsByClassName('remember_input');
+const signupForm = document.getElementById('signup__form');
+const loginForm = document.getElementById('login__form');
+const showSignUpBtn = document.getElementById('sign_up_ct_link');
+const showLoginBtn = document.getElementById('login_ct_link');
+const passWordStatus = document.getElementById('password_status');
 const audio = document.getElementsByTagName('audio').item(0);
 const audio_btn = document.getElementById('sound_effect_button');
 //enable tooltip
@@ -122,20 +132,99 @@ particlesJS('particles-js', {
 // particle effect
 // turn on and off the sound effect
 audio_btn.addEventListener('click', function () {
-  // alert(123);
-  // this.classList.toggle('fa-volume-xmark');
   if (!this.classList.contains('disable')) {
-    this.classList.add('disable','text-danger', 'bg-transparent');
+    this.classList.add('disable', 'text-danger', 'bg-transparent');
     this.firstChild.classList.add('fa-volume-xmark');
     audio.muted = true;
   } else {
-    this.classList.remove('disable','text-danger', 'bg-transparent');
+    this.classList.remove('disable', 'text-danger', 'bg-transparent');
     this.firstChild.classList.remove('fa-volume-xmark');
     audio.muted = false;
   }
 });
 // turn on and off the sound effect
 // audio actions
-audio.play();
-audio.loop = true;
+window.addEventListener('load', function () {
+  audio.play();
+  audio.loop = true;
+});
 // audio actions
+
+// input label animation & emptiness check
+inputs.forEach((input) => {
+  input.addEventListener('blur', function () {
+    passWordStatus.hidden = true;
+    if (this.value !== '') {
+      this.nextElementSibling.classList.add('filled');
+      this.parentElement.lastElementChild.hidden = true;
+      this.style.borderColor = '#404040';
+    } else {
+      this.parentElement.lastElementChild.hidden = false;
+      this.parentElement.lastElementChild.style.color = '#e13a5a';
+      const tooltip = new bootstrap.Tooltip(
+        this.parentElement.lastElementChild,
+        {
+          title: 'the value is empty !😑',
+          placement: 'right',
+          customClass: 'custom_tooltip',
+        }
+      );
+      this.style.borderColor = '#e13a5a';
+      this.nextElementSibling.classList.remove('filled');
+    }
+  });
+  //   
+  input.addEventListener('focus', function () {
+    if(this.type === 'password'){
+        if (this.value.length > 0) passWordStatus.hidden = false;
+        else passWordStatus.hidden = true;
+    }
+    this.parentElement.lastElementChild.hidden = true;
+    this.style.borderColor = 'peru';
+  });
+  if (input.type === 'password') {
+    input.addEventListener('input', function () {
+        passWordStatus.hidden = false;
+      if (input.value.length > 0 && input.value.length < 8) {
+        passWordStatus.style.color = '#fd7e14';
+        passWordStatus.innerHTML =
+          'password is weak <i class="fa-solid fa-circle-exclamation"></i>';
+      } else if (input.value.length >= 8) {
+        passWordStatus.innerHTML ='password is strong 💪';
+          passWordStatus.style.color = '#90ee90';
+      } else {
+        passWordStatus.hidden = true;
+      }
+    });
+  }
+});
+// show & hide password
+seePassIcons.forEach((seePassIcon) => {
+  seePassIcon.addEventListener('click', function () {
+    if (this.classList.contains('fa-eye-slash')) {
+      this.nextElementSibling.type = 'password';
+      this.classList.remove('fa-eye-slash');
+    } else {
+      this.nextElementSibling.type = 'text';
+      this.classList.add('fa-eye-slash');
+    }
+  });
+});
+// show & hide password
+
+// form animations
+showSignUpBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  loginForm.classList.add('animate__fadeOut');
+  signupForm.classList.remove('animate__backOutUp');
+  signupForm.classList.add('animate__backInDown');
+  signupForm.style.zIndex = 2;
+  signupForm.style.transform = 'translateY(0)';
+});
+showLoginBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  signupForm.classList.add('animate__backOutUp');
+  loginForm.classList.remove('animate__fadeOut');
+  loginForm.classList.add('animate__fadeIn');
+});
+// form animations
